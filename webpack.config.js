c745048ee9046eb273680bea68769fa60fdf11d7
Chenyt26 <2396164919@qq.com>
2022-06-01 16:10:59 +0800

    codecheck
    Match-id-690d614d72904b82aac6ca4bc392f5c968374ea2

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,9 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
+            /* Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+               the "scss" and "sass" values for the lang attribute to the right configs here.
+               other preprocessors should work out of the box, no loader config like this necessary. */
             'css': 'vue-style-loader!css-loader',
           }
           // other vue-loader options go here
